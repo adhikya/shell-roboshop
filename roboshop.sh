e17e17ac2bcee2439c6d6960218d5dabaccb1b2f
adhikya <ch.adhikya@gmail.com>
2025-05-25 09:04:46 +0800

    testing

---
+++ roboshop.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+AMI_ID="ami-09c813fb71547fc4f"
+SG_ID="sg-069a28d699aae9b90"
+INSTANCES=("mongodb" "redis" "mySQL" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "frontend")
+ZONE_ID="Z03545723JPI3NUQJEQ82"
+DOMAIN_NAME="adhikya.site"
+
+for instance in ${INSTANCES[@]}
+do 
+    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-9c813fb71547fc4f --instance-type t2.
+    micro --security-group-ids sg-069a28d699aae9b90 --tag-specifications "ResourceType=instance, 
+    Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
+    if [$instance != "frontend"]
+    then 
+         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
+    else
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)     
+
+  
+
+
+done  
